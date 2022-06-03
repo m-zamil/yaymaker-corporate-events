4a589d7d474cf722e62489d12ed39c12183e564a
Muzamil Khan <muzamil-JavaScriptDev@outlook.com>
2022-06-03 23:03:57 +0500

    How it works section

--- assets/main.js
+++ assets/main.js
@@ -1,89 +1,91 @@
-//initialize slider
-new Glide(".glide", {
-  type: "carousel",
-  gap: 10,
-  autoplay: 4000,
-  hoverpause: true,
-  keyboard: true,
-  loop: false,
-  perView: 1.3,
-  peek: {
-    before: 0,
-    after: 50,
-  },
-  breakpoints: {
-    19200: {
-      perView: 4,
-      peek: {
-        before: 0,
-        after: 0,
-      },
-      autoplay: false,
-      gap: 20,
-      loop: false,
+window.onload = function () {
+  //initialize slider
+  new Glide(".glide", {
+    type: "carousel",
+    gap: 10,
+    autoplay: 4000,
+    hoverpause: true,
+    keyboard: true,
+    loop: false,
+    perView: 1.3,
+    peek: {
+      before: 0,
+      after: 50,
     },
-    960: {
-      perView: 1,
-      peek: {
-        before: 60,
-        after: 60,
+    breakpoints: {
+      19200: {
+        perView: 4,
+        peek: {
+          before: 0,
+          after: 0,
+        },
+        autoplay: false,
+        gap: 20,
+        loop: false,
       },
-      gap: 15,
+      960: {
+        perView: 1,
+        peek: {
+          before: 60,
+          after: 60,
+        },
+        gap: 15,
+      },
     },
-  },
-}).mount();
+  }).mount();
 
-//initialize slider
-new Glide(".glideTopexp", {
-  type: "carousel",
-  gap: 10,
-  autoplay: 4000,
-  hoverpause: true,
-  keyboard: true,
-  perView: 1,
-  peek: {
-    before: 0,
-    after: 50,
-  },
-  breakpoints: {
-    19200: {
-      perView: 3,
-      peek: {
-        before: 0,
-        after: 0,
-      },
-      gap: 0,
+  //initialize slider
+  new Glide(".glideTopexp", {
+    type: "carousel",
+    gap: 10,
+    autoplay: 4000,
+    hoverpause: true,
+    keyboard: true,
+    perView: 1,
+    peek: {
+      before: 0,
+      after: 50,
     },
-
-    960: {
-      perView: 1.3,
-      peek: {
-        before: 0,
-        after: 0,
+    breakpoints: {
+      19200: {
+        perView: 3,
+        peek: {
+          before: 0,
+          after: 0,
+        },
+        gap: 0,
       },
-      gap: 0,
-    },
-  },
-}).mount();
 
-if (screen.width > 960) {
-  const everyLi = document.querySelectorAll(".every__slider li:not(.glide__slide--clone)");
-  if (everyLi.length > 4) {
-    console.log(everyLi.length);
-    document.querySelector(".controls").style.display = "block";
+      960: {
+        perView: 1.3,
+        peek: {
+          before: 0,
+          after: 0,
+        },
+        gap: 0,
+      },
+    },
+  }).mount();
+
+  if (screen.width > 960) {
+    const everyLi = document.querySelectorAll(".every__slider li:not(.glide__slide--clone)");
+    if (everyLi.length > 4) {
+      console.log(everyLi.length);
+      document.querySelector(".controls").style.display = "block";
+    }
   }
-}
 
-//initialize slider
-new Glide(".enhanceSlider", {
-  type: "carousel",
-  gap: 0,
-  autoplay: 4000,
-  hoverpause: true,
-  keyboard: true,
-  perView: 1,
-  peek: {
-    before: 0,
-    after: 0,
-  },
-}).mount();
+  //initialize slider
+  new Glide(".enhanceSlider", {
+    type: "carousel",
+    gap: 0,
+    autoplay: 4000,
+    hoverpause: true,
+    keyboard: true,
+    perView: 1,
+    peek: {
+      before: 0,
+      after: 0,
+    },
+  }).mount();
+};
